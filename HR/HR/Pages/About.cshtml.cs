e043f268eb99fad1a79c4037e01f94c72aa1f426
Eugene Bykov <eugeneby@microsoft.com>
2019-04-24 14:20:25 -0700

    Design updates and cleanup

--- HR/HR/Pages/About.cshtml.cs
+++ HR/HR/Pages/About.cshtml.cs
@@ -8,11 +8,5 @@ namespace HR.Pages
 {
     public class AboutModel : PageModel
     {
-        public string Message { get; set; }
-
-        public void OnGet()
-        {
-            Message = "Your application description page.";
-        }
     }
 }
